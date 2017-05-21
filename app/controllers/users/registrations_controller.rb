83ac783744c7ad6dd21da57ac80d1b8b61ec319a
Sage Beard <sagek711@gmail.com>
2017-05-20 19:19:36 -0700

    user authentication complete

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,34 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
-
-  def configure_sign_up_params
-     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
-
-    if @user.save
-      flash[:notice] = "Welcome to Blockipedia #{@user.name}!"
-      create_session(@user)
-      redirect_to root_path
-    else
-      flash.now[:alert] = "There was an error creating your account. Please try again."
-      render :new
-    end
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
+  #before_action :configure_sign_up_params, only: [:create]
+  #before_action :configure_account_update_params, only: [:update]
 end
