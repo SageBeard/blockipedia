944306737e0be11f400435663889e41ac4a40e38
Sage Beard <sagek711@gmail.com>
2017-06-01 21:08:02 -0700

    randomdata & seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+# Create Wikis
+50.times do
+# #1
+  Wiki.create!(
+# #2
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+  wikis = Wiki.all
+
+# Create Comments
+# #3
+100.times do
+  Comment.create!(
+# #4
+    wiki: Wikis.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Wiki.count} wikis created"
+puts "#{Comment.count} comments created"
