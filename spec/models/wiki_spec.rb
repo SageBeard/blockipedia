77a41147f87b0157b2030f6efb2b590dd6bc662a
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-06-29 21:19:24 -0700

    pass the wiki tests

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe Wiki, type: :model do
-  let(:wiki) { Wiki.create!(title: "My String", body: "New Wiki Body") }
-  let(:user) { User.create!(name: RandomData.random_name, password: RandomData.random_sentence, password_confirmation: RandomData.random_sentence, email: "foo9@factory.com")}
-  #
-  # describe "attributes" do
-  #   it "has title and body attributes" do
-  #     expect(wiki).to have_attributes(user: user title: "New Wiki Title", body: "New Wiki Body")
-  #   end
-  # end
+  password = RandomData.random_sentence
+  let(:user) {
+    User.create!(
+      password: password,
+      password_confirmation: password,
+      email: "foo9@factory.com"
+    )
+  }
+  let(:wiki) { Wiki.create!(title: "My String", body: "New Wiki Body", user: user) }
 
-  #
-  # describe "wiki scope works" do
-  #   it "filters down wikis" do
-  #     expect(Wiki.all.visible_to(user).count).to eq(Wiki.all.count)
-  #   end
-  # end
+  describe "attributes" do
+    it "has title and body attributes" do
+      expect(wiki).to have_attributes(user: user, title: "My String", body: "New Wiki Body")
+    end
+  end
+
+  describe "wiki scope works" do
+    it "filters down wikis" do
+      expect(Wiki.all.visible_to(user).count).to eq(Wiki.all.count)
+    end
+  end
 end
