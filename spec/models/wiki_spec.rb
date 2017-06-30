c19fc9acc29d3e8a25ee6e706f848367301d183f
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-06-29 21:28:25 -0700

    adds scope test

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -3,14 +3,16 @@ require 'pry'
 
 RSpec.describe Wiki, type: :model do
   password = RandomData.random_sentence
-  let(:user) {
+  let!(:user) {
     User.create!(
       password: password,
       password_confirmation: password,
-      email: "foo9@factory.com"
+      email: "first@factory.com"
     )
   }
-  let(:wiki) { Wiki.create!(title: "My String", body: "New Wiki Body", user: user) }
+
+  let!(:wiki) { Wiki.create!(title: "My String", body: "New Wiki Body", user: user) }
+  let!(:private_wiki) { Wiki.create!(title: "My String", body: "New Wiki Body", user: user, private: true) }
 
   describe "attributes" do
     it "has title and body attributes" do
@@ -18,9 +20,11 @@ RSpec.describe Wiki, type: :model do
     end
   end
 
-  describe "wiki scope works" do
+  describe "wiki #visible_to scope works" do
     it "filters down wikis" do
-      expect(Wiki.all.visible_to(user).count).to eq(Wiki.all.count)
+      # Since the user is not premium they will only be able
+      # to see the non-private wiki
+      expect(Wiki.all.visible_to(user).count).to eq(1)
     end
   end
 end
